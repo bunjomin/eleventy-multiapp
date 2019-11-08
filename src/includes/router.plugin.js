07503011c609ef3f7aeee13b44d8b21cf49e7e27
benniBoi <bharrington11@gmail.com>
2019-11-07 22:20:34 -0500

    Build out basic routing functionality

--- src/includes/router.plugin.js
+++ src/includes/router.plugin.js
@@ -1,9 +1,22 @@
-const router = () => {
-  return {
-    install: function (Vue, options) {
-      Vue.setRoute = function (route) {
-        window.bunjomin.route = route;
-      }
+const router = {
+  install: function (Vue, options) {
+    Vue.prototype.$setRoute = function (targetApp) {
+      window.bunjomin.route = targetApp.permalink;
+      console.log(targetApp.permalink);
+      window.history.pushState(null, targetApp.permalink, targetApp.permalink);
+    },
+    Vue.prototype.$getRoute = async function (targetApp) {
+      const response = await fetch(targetApp.permalink).catch((err) => console.log(err)),
+        body = await response.text(),
+        parser = new DOMParser(),
+        doc = parser.parseFromString(body, 'text/html'),
+        selectedScript = doc.getElementById(targetApp.slug),
+        newScript = document.createElement('script'),
+        inline = document.createTextNode(selectedScript.innerHTML);
+      newScript.appendChild(inline);
+      newScript.id = targetApp.slug;
+      newScript.type = 'application/javascript';
+      document.body.appendChild(newScript);
     }
   }
 };
